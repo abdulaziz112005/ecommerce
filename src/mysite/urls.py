13828494f08137fb26617ddc81ca5b905e5a55a5
7ahongir <jnytuit@mail.ru>
2020-12-25 21:11:48 +0500

    product detail

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('detail/', include('product.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/product/views.py
+++ src/product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import *
+def detail(request, id):
+    try:
+        detail = Product.objects.get(pk=id);
+    except Product.DoesNotExist:
+        detail = None
+    context= {"product": detail}
+    return render(request, 'product/detail.html', context)
